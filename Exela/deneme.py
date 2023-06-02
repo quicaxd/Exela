f2311b3e6dceeb47b2646aa5ba6b3d8c975164b9
quicaxd <127431653+quicaxd@users.noreply.github.com>
2023-06-02 09:51:33 +0300

    Delete deneme.py

--- Exela/deneme.py
+++
@@ -1,7 +0,0 @@
-hook = str(input("Enter your webhook URL : "))
-
-
-file = open(r"C:\Users\anani\OneDrive\Masaüstü\deneme.py", "r")
-source = file.read()
-updatesource = source.replace("%URL%", hook)
-print(updatesource)
